f2cdbf9eeb1c0506a0511bdfbf9c611b7ec6df15
Park youngho <snu910501@naver.com>
2023-04-28 20:59:02 +0900

    multer

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -2,11 +2,26 @@ const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
+const multer = require('multer');
 
 const LoginController = require('../controllers/login.controller');
 const loginController = new LoginController();
 
-router.post('/', (req, res, next) => {
+const upload = multer({
+  storage: multer.diskStorage({
+    // destination: function (req, file, cb) {
+    //   cb(null, "./uploads");
+    // },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+  limits: {
+    fileSize: 100 * 1024 * 1024,
+  },
+});
+
+router.post('/', upload.array('images', 5), (req, res, next) => {
   passport.authenticate('local', { session: false }, (err, user, info) => {
     if (err) {
       return next(err);
